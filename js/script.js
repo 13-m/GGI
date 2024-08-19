998d65ec850a4f63b49bac7bd07a1c5ac6430d78
13-m <igor.suxarev.20@mail.ru>
2024-08-19 14:01:02 +0300

    feat: add gitignore and fix input date

--- js/script.js
+++ js/script.js
@@ -1,62 +1,56 @@
 var dt = new DataTransfer();
 
-// document.querySelectorAll(".input-file input[type=file]").forEach((input) => {
-//   input.addEventListener("change", function () {
-//     let filesList = this.closest(".input-file").nextElementSibling;
-//     filesList.innerHTML = ""; // Очистить текущий список файлов
+// toggle/switch
+const option1 = document.getElementById("option1");
+const option2 = document.getElementById("option2");
+const sectionAbit = document.getElementById("abit");
+const sectionDiploma = document.getElementById("diplomSec");
+const sectionScientist = document.getElementById("scientist");
+const sectionAdditional = document.getElementById("additional");
 
-//     Array.from(this.files).forEach((file) => {
-//       let fileItem = document.createElement("div");
-//       fileItem.classList.add("input-file-list-item");
+function curSelect() {
+  if (option1.checked) {
+    console.log("Current select: Абитуриент");
+    sectionAbit.classList.toggle("hidden");
+    sectionDiploma.classList.toggle("hidden");
+    sectionScientist.classList.toggle("hidden");
+    sectionAdditional.classList.toggle("hidden");
+  } else if (option2.checked) {
+    sectionAbit.classList.toggle("hidden");
+    sectionDiploma.classList.toggle("hidden");
+    sectionScientist.classList.toggle("hidden");
+    sectionAdditional.classList.toggle("hidden");
 
-//       let fileName = document.createElement("span");
-//       fileName.classList.add("input-file-list-name");
-//       fileName.textContent = file.name;
+    console.log("Current select: Аспирант");
+  }
+}
+option1.addEventListener("change", curSelect);
+option2.addEventListener("change", curSelect);
 
-//       let removeLink = document.createElement("a");
-//       removeLink.href = "#";
-//       removeLink.textContent = "x";
-//       removeLink.classList.add("input-x-list-remove");
-//       removeLink.addEventListener("click", (e) => {
-//         e.preventDefault();
-//         removeFilesItem(file.name, this);
-//       });
+const select = document.getElementById("date-select");
+for (let year = 2024; year >= 1910; year--) {
+  const option = document.createElement("option");
+  option.value = year;
+  option.text = year;
+  select.appendChild(option);
+}
 
-//       fileItem.appendChild(fileName);
-//       fileItem.appendChild(removeLink);
-//       filesList.appendChild(fileItem);
+const choices = new Choices(select, {
+  searchEnabled: false,
+  itemSelectText: "",
+  sorter: function (a, b) {
+    return b.label.length - a.label.length;
+  },
+});
 
-//       dt.items.add(file);
-//     });
+for (let year = 1920; year <= 2025; year++) {
+  let option = document.createElement("option");
+  option.value = year;
+  option.innerHTML = year;
+  document.getElementById("date-select").appendChild(option);
+}
 
-//     this.files = dt.files;
-//   });
-// });
-
-// function removeFilesItem(name, input) {
-//   let filesList = input.closest(".input-file").nextElementSibling;
-//   let fileItems = filesList.querySelectorAll(".input-file-list-item");
-
-//   // Удалить элемент из списка
-//   fileItems.forEach((item) => {
-//     let fileItemName = item.querySelector(".input-file-list-name").textContent;
-//     if (fileItemName === name) {
-//       filesList.removeChild(item);
-//     }
-//   });
-
-//   // Обновить DataTransfer объект
-//   let newFiles = [];
-//   Array.from(dt.items).forEach((item) => {
-//     if (item.getAsFile().name !== name) {
-//       newFiles.push(item.getAsFile());
-//     }
-//   });
-
-//   dt.items.clear();
-//   newFiles.forEach((file) => dt.items.add(file));
-//   input.files = dt.files; // Обновить поле ввода
-// }
+// end toggle
 
 const max_files = 10;
 document.querySelectorAll(".input-file input[type=file]").forEach((input) => {
